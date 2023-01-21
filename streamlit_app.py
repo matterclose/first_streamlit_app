e0782dc9c2e36bcec373235462c3b2bbc68dcca8
matterclose <123140518+matterclose@users.noreply.github.com>
2023-01-22 00:27:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,8 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return (fruityvice_normalized)
+except URLError as e: 
+streamlit.error()
    
 
 streamlit.header("Fruityvice Fruit Advice!")
